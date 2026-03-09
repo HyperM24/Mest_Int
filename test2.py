40ae7d735b937cf48e9f2998f81fce75e9487779
Sir Kanalas Leonárdó <82200593+HyperM24@users.noreply.github.com>
2026-03-09 06:24:48 +0100

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+
+class Kiralynok:
+    def __init__(self, ke, c):
+        self.kezdo = ke  # (tábla, kezdő sor)
+        self.cel = c  # utolsó sor index
+        self.n = len(ke[0])  # tábla mérete (8)
+
+    def celteszt(self, a):
+        # ha az aktuális sor = tábla mérete, minden királynő elhelyezve
+        return a[1] == self.n
+
+    def rakovetkezo(self, t):
+        gyerekek = []
+        s = t[1]  # aktuális sor
+        a = t[0].copy()
+
+        for i in range(self.n):
+            elofeltetel = True
+
+            # oszlop ellenőrzés
+            for sor in range(s):
+                if a[sor, i] == 1:
+                    elofeltetel = False
+                    break
+
+            # diagonális ellenőrzés abs segítségével
+            if elofeltetel:
+                for sor in range(s):
+                    for oszlop in range(self.n):
+                        if a[sor, oszlop] == 1 and abs(s - sor) == abs(i - oszlop):
+                            elofeltetel = False
+                            break
+                    if not elofeltetel:
+                        break
+
+            if elofeltetel:
+                uj_allpot = a.copy()
+                uj_allpot[s, i] = 1
+                gyerekek.append((uj_allpot, s + 1))
+
+        return gyerekek
+
+
+def main():
+    tabla = np.zeros((8, 8), dtype=int)
+    feladat = Kiralynok((tabla, 0), 8)  # kezdő sor 0, cél: 8 (az összes sor betöltve)
+
+    allapotok = [feladat.kezdo]
+
+    while allapotok:
+        uj_allapot = []
+        for a in allapotok:
+            if feladat.celteszt(a):
+                print(a[0])
+                return
+            uj_allapot += feladat.rakovetkezo(a)
+        allapotok = uj_allapot
+
+
+if __name__ == "__main__":
+    main()
